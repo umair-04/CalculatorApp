b1bfdb7b2913a65555c5ede309c9c102fc5a5d49
umair <bcsf17a504@pucit.edu.pk>
2020-10-03 19:06:40 -0700

    buttons and editbox variables created with values

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn0,btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btnAdd,btnSub,btnMul,btnDiv,btnClear,btnPoint;
+    Button btn0,btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btnAdd,btnSub,btnMul,btnDiv,btnClear,btnPoint,btnEqual;
     EditText ed1;
     boolean add,sub,mul,div;
 
@@ -16,5 +16,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn0 = (Button)findViewById(R.id.btn0);
+        btn1 = (Button)findViewById(R.id.btn1);
+        btn2 = (Button)findViewById(R.id.btn2);
+        btn3 = (Button)findViewById(R.id.btn3);
+        btn4 = (Button)findViewById(R.id.btn4);
+        btn5 = (Button)findViewById(R.id.btn5);
+        btn6 = (Button)findViewById(R.id.btn6);
+        btn7 = (Button)findViewById(R.id.btn7);
+        btn8 = (Button)findViewById(R.id.btn8);
+        btn9 = (Button)findViewById(R.id.btn9);
+        btnAdd = (Button)findViewById(R.id.btnAdd);
+        btnSub = (Button)findViewById(R.id.btnSub);
+        btnMul = (Button)findViewById(R.id.btnMul);
+        btnDiv = (Button)findViewById(R.id.btnDiv);
+        btnClear = (Button)findViewById(R.id.btnClear);
+        btnPoint = (Button)findViewById(R.id.btnPoint);
+        btnEqual = (Button)findViewById(R.id.btnEqual);
+
+        ed1 = (EditText)findViewById(R.id.ed1);
+
+
+
     }
 }
