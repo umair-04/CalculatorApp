97bbc4a40e97f88dbd2cb8b032152c1c91df4fb9
umair <bcsf17a504@pucit.edu.pk>
2020-10-03 18:43:27 -0700

    layout designed and button and text objects created

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.mycalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn0,btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btnAdd,btnSub,btnMul,btnDiv,btnClear,btnPoint;
+    EditText ed1;
+    boolean add,sub,mul,div;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
